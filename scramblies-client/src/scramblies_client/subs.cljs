8ff3d3dfaa9444c6b9f7b2e14d6f78c560827632
Kruzabasi <kruzabasi@gmail.com>
2022-07-17 13:59:57 +0100

    register subs for input values and api res

--- scramblies-client/src/scramblies_client/subs.cljs
+++ scramblies-client/src/scramblies_client/subs.cljs
@@ -1,8 +1,23 @@
 (ns scramblies-client.subs
   (:require
-   [re-frame.core :as re-frame]))
+   [re-frame.core :refer [reg-sub]]))
 
-(re-frame/reg-sub
+(reg-sub
  ::name
  (fn [db]
    (:name db)))
+
+(reg-sub
+ :str1-value
+ (fn [db]
+   (:str1 db)))
+
+(reg-sub
+ :str2-value
+ (fn [db]
+   (:str2 db)))
+
+(reg-sub
+ :scramblies-res
+ (fn [db]
+   (:scramblies-res db)))
